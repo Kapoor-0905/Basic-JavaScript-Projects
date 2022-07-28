606de7fc4586cdafa579206925403562dc9d078c
Kapoor-0905 <500097111@stu.upes.ac.in>
2022-07-28 23:20:49 +0530

    chrome extension added

---
+++ Chrome Extension/index.js
@@ -0,0 +1,8 @@
+function saveinput() {
+    console.log("Button clicked!!")
+}
+
+let myLeads = [];
+const inputEl = Document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", saveinput)
